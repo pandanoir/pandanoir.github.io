972cde58df687a2e5c8570b80561459f7bae2fe4
pandanoir <moon_in_france@yahoo.co.jp>
2017-11-26 20:44:26 +0900

    fix scene.js

--- scene.js
+++ scene.js
@@ -1,9 +1,16 @@
-riot.tag2('scene', '<div show="{show}"> <yield></yield> </div>', '', '', function(opts) {
-    if (opts.start) this.show=false;
+riot.tag2('scene', '<div if="{show}"> <yield></yield> </div>', '', '', function(opts) {
+    if (opts.start) {
+      this.show = false;
+      setTimeout(() => {
+        this.show = true;
+        this.update();
+      }, opts.start);
+    }
     else this.show=true;
     if (opts.timeout) {
       setTimeout(() => {
         this.unmount();
+        this.update();
       }, opts.timeout);
     }
 });
